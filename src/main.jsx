9dc6ccc05529cd581f50df361d329761b0dc64ce
wavemeetweb <abhilash123happy@gmail.com>
2026-03-26 13:21:33 +0530

    Update and rename Main.jsx to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // ⚠️ must match filename EXACTLY
 import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
