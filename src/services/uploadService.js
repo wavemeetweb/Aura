cb3587a2404b3f0bc8fbd85f910fa14810fbc43d
wavemeetweb <abhilash123happy@gmail.com>
2026-03-26 11:53:20 +0530

    Update uploadService.js

--- src/services/uploadService.js
+++ src/services/uploadService.js
@@ -3,12 +3,14 @@ import axios from "axios";
 export const uploadImage = async (file) => {
   const form = new FormData();
   form.append("file", file);
-  form.append("upload_preset", "YOUR_UNSIGNED_PRESET");
+  form.append("upload_preset", "aura_unsigned"); // your preset
 
   const res = await axios.post(
-    "https://api.cloudinary.com/v1_1/dx7tyxquo/image/upload",
+    "https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload",
     form
   );
+
+  console.log(res.data); // 👈 ADD THIS
 
   return res.data.secure_url;
 };
